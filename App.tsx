9896bea118cb06db50e8854b1c4a3712bf658d3e
rahul parida <rparida1052@gmail.com>
2024-08-23 23:30:46 +0530

    iniit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Alert, Pressable, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+
+  const handleDelete = ()=>{
+    Alert.alert("Are you sure you want to delete this item?", "It will good", [
+      {
+        text: "Yes",
+        onPress: () => console.log("Deleting"),
+        style: "destructive",
+      },
+      {
+        text: "Cancel",
+        onPress: () => console.log("cancel"),
+        style: "cancel",
+      },
+    ]);
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <View style={styles.itemContainer}>
+        <Text style={{ fontSize: 18, fontWeight: "semibold" }}>Coffee</Text>
+        <TouchableOpacity style={styles.deleteButton} onPress={handleDelete}>
+          <Text style={{color:"white",textTransform:"uppercase",letterSpacing:1}}>Delete</Text>
+        </TouchableOpacity>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +33,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    justifyContent: "center",
   },
+  itemContainer: {
+    borderBottomWidth: 1,
+    borderBottomColor: "1a759f",
+    paddingHorizontal: 10,
+    paddingVertical: 10,
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  deleteButton:{
+    backgroundColor:"black",
+    padding:8,
+    borderRadius:6,
+  }
 });
---
+++ notes/lec1.md
@@ -0,0 +1,11 @@
+```shell
+yarn lint --fix
+```
+while specifing the attributes like border ratio or padding 
+we define like paddingHorizontal:8   // Here 8 is not show pixel or any other unit rather it is a number or square matrix of pixel size
+
+we can get out pixel size by using // PixelRatio.get() :// here pixelRatio comes from react-native
+
+
+## TIPS:- 
+For properties like border and borderRadius try to use even numbers for betterr result.
