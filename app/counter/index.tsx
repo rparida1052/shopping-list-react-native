37f97db51184231bbceb5ad84da601571161962e
rahul parida <rparida1052@gmail.com>
2024-09-06 20:56:19 +0530

    Add a countdown timer

--- app/counter/index.tsx
+++ app/counter/index.tsx
@@ -1,10 +1,22 @@
 import { Alert, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { theme } from '../../theme'
 import { registerForPushNotificationsAsync } from '../../utils/registerForPushNotificationsAsync'
 import * as Notifications from "expo-notifications"
+import { intervalToDuration, isBefore } from 'date-fns'
+import { TimeSegment } from '../../components/TimeSegment'
 
+type CoundownStatus = {
+  isOverdue: boolean;
+  distance: ReturnType<typeof intervalToDuration>
+}
+const timeStamp = Date.now() + 10*1000; // 10 seconds from now
 const Counter = () => {
+  const [status, setStatus] = useState<CoundownStatus>({
+    isOverdue:false,
+    distance:{}
+  })
+  const [secondElapsed, setSecondElapsed] = useState(0);
   const handlePuchNotification = async () =>{
     const result = await registerForPushNotificationsAsync();
     if(result === "granted"){
@@ -20,13 +32,65 @@ const Counter = () => {
       Alert.alert("Permission not granted","Please enalbe the permission to receive notification")
     }
   }
+  
+  useEffect(()=>{
+    const intervalId = setInterval(()=>{
+      const isOverdue = isBefore(timeStamp,Date.now());
+      const distance = intervalToDuration(
+        isOverdue ? {start : timeStamp,end:Date.now()} :{
+          start:Date.now(),
+          end:timeStamp
+        }
+      )
+      setStatus({isOverdue,distance})
+    },1000);
+    return () => clearInterval(intervalId);
+  },[])
+  console.log(status);
+  
   return (
-    <View style={styles.styleContainer}>
-      <TouchableOpacity style={styles.buttonContainer} onPress={handlePuchNotification}>
+    <View
+      style={[
+        styles.styleContainer,
+        status.isOverdue ? styles.lateBg : undefined,
+      ]}>
+      {status.isOverdue ? (
+        <Text style={status.isOverdue ? styles.lateText : undefined}>
+          Things overdue by :{" "}
+        </Text>
+      ) : (
+        <Text>Things due in ... </Text>
+      )}
+
+      <View style={styles.rowContainer}>
+        <TimeSegment
+          unit="Days"
+          number={status.distance.days ?? 0}
+          textStyle={status.isOverdue ? styles.lateText : undefined}
+        />
+        <TimeSegment
+          unit="Hours"
+          number={status.distance.hours ?? 0}
+          textStyle={status.isOverdue ? styles.lateText : undefined}
+        />
+        <TimeSegment
+          unit="Minutes"
+          number={status.distance.minutes ?? 0}
+          textStyle={status.isOverdue ? styles.lateText : undefined}
+        />
+        <TimeSegment
+          unit="Seconds"
+          number={status.distance.seconds ?? 0}
+          textStyle={status.isOverdue ? styles.lateText : undefined}
+        />
+      </View>
+      <TouchableOpacity
+        style={styles.buttonContainer}
+        onPress={handlePuchNotification}>
         <Text style={styles.buttonText}>Schedule Notification</Text>
       </TouchableOpacity>
     </View>
-  )
+  );
 }
 
 export default Counter
@@ -37,6 +101,14 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
   },
+  lateBg:{
+    backgroundColor:theme.colorRed
+  },
+  rowContainer :{
+    flexDirection:"row",
+    justifyContent:"space-between",
+    padding:10
+  },
   buttonContainer:{
     backgroundColor:theme.colorCerulean,
     padding:10,
@@ -45,5 +117,8 @@ const styles = StyleSheet.create({
   buttonText:{
     color:theme.colorWhite,
     letterSpacing:1
+  },
+  lateText:{
+    color:theme.colorWhite
   }
 })
--- theme.ts
+++ theme.ts
@@ -2,4 +2,5 @@ export const theme = {
   colorCerulean: "#1a759f",
   colorWhite: "#ffffff",
   colorGrey: "#D3D3D3",
+  colorRed: "#DF0000",
 };
